100a8ac6a0019cc6ab8083a33705cddd51217c91
ElitsaDev <94979851+ElitsaDev@users.noreply.github.com>
2022-12-11 17:37:48 +0200

    Show games in catalog

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
+import {useEffect, useState} from 'react';
 import { Route, Routes } from 'react-router-dom';
+import * as gameService from './services/gameService';
 
 import { Header } from './components/Header/Header';
 import { Home } from './components/Home/Home';
@@ -6,25 +8,32 @@ import { Login } from './components/Login/Login';
 import { Register } from './components/Register/Register';
 import { CreateGame } from './components/CreateGame/CreateGame';
 import { EditGame } from './components/EditGame/EditGame';
-
-import './App.css';
 import { Catalog } from './components/Catalog/Catalog';
 import { Details } from './components/Details/Details';
+import './App.css';
 
 function App() {
+    const [games, setGames] = useState([]);
+
+    useEffect(() => {
+        gameService.getAll()
+        .then(result => {
+            setGames(result);
+        })
+    },[]);
     return (
         <div id="box">
             < Header />
             {/* Main Content */}
             <main id="main-content">
                 <Routes>
-                    <Route path="/" element={< Home />}/>
+                    <Route path="/" element={< Home games={games}/>}/>
                     <Route path="/login" element={< Login />}/>
                     <Route path="/register" element={< Register />}/>
                     <Route path="/create" element={< CreateGame />}/>
                     <Route path="/create" element={< EditGame />}/>
                     <Route path="/details" element={< Details />}/>
-                    <Route path="/catalog" element={< Catalog />}/>
+                    <Route path="/catalog" element={< Catalog games={games}/>}/>
                 </Routes>
             </main>
             
